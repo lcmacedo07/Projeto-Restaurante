0c9992cd9f9c35503b98c3eabe8d43e8c8fd4599
MACEDO' Gerisvaldo <you@example.com>
2018-11-22 00:12:33 -0200

    first commit

---
+++ resources/views/admin/item/create.blade.php
@@ -0,0 +1,77 @@
+@extends('layouts.app')
+
+@section('title','Create')
+
+@push('css')
+
+@endpush
+
+@section('content')
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    @include('layouts.partial.msg')
+                    <div class="card">
+                        <div class="card-header" data-background-color="purple">
+                            <h4 class="title">Adicionar Novo Item</h4>
+                        </div>
+                        <div class="card-content">
+                            <form method="POST" action="{{ route('item.store') }}" enctype="multipart/form-data">
+                                @csrf
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group label-floating">
+                                            <label class="control-label">Categoria</label>
+                                            <select class="form-control" name="category">
+                                                @foreach($categories as $category)
+                                                    <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group label-floating">
+                                            <label class="control-label">Nome</label>
+                                            <input type="text" class="form-control" name="name">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group label-floating">
+                                            <label class="control-label">Descrição</label>
+                                            <textarea class="form-control" name="description"></textarea>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group label-floating">
+                                            <label class="control-label">Preço</label>
+                                            <input type="number" class="form-control" name="price">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                            <label class="control-label">Imagem</label>
+                                            <input type="file" name="image">
+                                    </div>
+                                </div>
+                                <a href="{{ route('item.index') }}" class="btn btn-danger">Voltar</a>
+                                <button type="submit" class="btn btn-primary">Salvar</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('scripts')
+
+@endpush
---
+++ resources/views/layouts/partial/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <p class="copyright pull-right">
+            &copy;
+            <script>
+                document.write(new Date().getFullYear())
+            </script>
+            Design por <a href="#">Criativo Tim</a>, e Desenvolvido por <a href="#">Leandro Macedo</a>
+        </p>
+    </div>
+</footer>
